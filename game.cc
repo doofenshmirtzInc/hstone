e460d262c92721ffaa9ffb2213f34206f3106388
jack <darkmatterdontmatter@protonmail.com>
2021-05-26 16:42:02 -0500

    first commit

---
+++ game.cc
@@ -0,0 +1,24 @@
+
+/*
+   class specification outline courtesy of http://www.geeksforgeeks.org/c-classes-and-objects
+   class ClassName{
+   	accessSpecifier
+	dataMembers
+	memberFunctions
+   }
+*/
+
+
+
+
+class Game{
+	private:
+		string name;
+
+
+	public:
+		// constuctor
+		Game(){ name = "Hearthstone" }
+
+
+};
---
+++ main.cc
@@ -0,0 +1,26 @@
+// this is a c++ file
+// .cc is an alternate extension for .cpp
+
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int
+main(){
+
+	// game loop
+	while(True){
+		// game loop steps ( according to game loop tut )
+		// while(userNotQuitOrCrash)
+		// 1. updatePhysics()
+		// 2. Scene.updateScenePicture()
+		// 3. passToNOdeScripts(userInput) -> update according to user input
+		// 4. performanceUPdate()
+		// 5. userNOtQuitOrCrash = updateAppState()
+	}
+
+	return 0;
+}
